e0e207e643661f74af6a78f48d94c31cf8eae081
OlegKolchin <commshepard@gmail.com>
2022-02-13 17:49:11 +0500

    Init

--- src/main/java/job4j/ru/job4j_passport/repository/PassportRepository.java
+++ src/main/java/job4j/ru/job4j_passport/repository/PassportRepository.java
@@ -4,10 +4,12 @@ import job4j.ru.job4j_passport.domain.Passport;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface PassportRepository extends CrudRepository<Passport, Integer> {
     List<Passport> findPassportBySeries(int series);
 
